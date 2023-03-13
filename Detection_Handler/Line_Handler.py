e2668d36ae9f27463f68772173fcb3ed20fbcb8b
amirkirshen <88038376+amirkirshen@users.noreply.github.com>
2023-03-13 18:59:27 +0200

    Version 0.0.1 - line detections

---
+++ Detection_Handler/Line_Handler.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+
+def Find_Lines():
+    # Load the image
+    # Reading the required image in
+    # which operations are to be done.
+    # Make sure that the image is in the same
+    # directory in which this python program is
+    # Read image
+    image = cv2.imread('Resources/Amir_Test/lines.jpg')
+
+    # Convert image to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Use canny edge detection
+    edges = cv2.Canny(gray, 50, 150, apertureSize=3)
+
+    # Apply HoughLinesP method to
+    # to directly obtain line end points
+    lines_list = []
+    lines = cv2.HoughLinesP(
+        edges,  # Input edge image
+        1,  # Distance resolution in pixels
+        np.pi / 180,  # Angle resolution in radians
+        threshold=40,  # Min number of votes for valid line
+        minLineLength=10,  # Min allowed length of line
+        maxLineGap=10  # Max allowed gap between line for joining them
+    )
+
+    # Iterate over points
+    for points in lines:
+        # Extracted points nested in the list
+        x1, y1, x2, y2 = points[0]
+        # Draw the lines joing the points
+        # On the original image
+        cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        # Maintain a simples lookup list for points
+        lines_list.append([(x1, y1), (x2, y2)])
+
+    # Save the result image
+    cv2.imshow("Detected lines", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
