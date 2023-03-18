21b3d1a4b9efe5d355c77f080d5410524480497d
amirkirshen <88038376+amirkirshen@users.noreply.github.com>
2023-03-18 17:14:52 +0200

    Parse a video to an Image Adding matrix for driving logic

--- Detection_Handler/Line_Handler.py
+++ Detection_Handler/Line_Handler.py
@@ -1,14 +1,15 @@
 import cv2
 import numpy as np
 
-def Find_Lines():
+def Find_Lines(frame, matrix, frameSize):
     # Load the image
     # Reading the required image in
     # which operations are to be done.
     # Make sure that the image is in the same
     # directory in which this python program is
     # Read image
-    image = cv2.imread('Resources/Amir_Test/lines.jpg')
+    # image = cv2.imread('Resources/Amir_Test/lines.jpg')
+    image = frame
 
     # Convert image to grayscale
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -23,7 +24,7 @@ def Find_Lines():
         edges,  # Input edge image
         1,  # Distance resolution in pixels
         np.pi / 180,  # Angle resolution in radians
-        threshold=40,  # Min number of votes for valid line
+        threshold=80,  # Min number of votes for valid line
         minLineLength=10,  # Min allowed length of line
         maxLineGap=10  # Max allowed gap between line for joining them
     )
@@ -32,6 +33,9 @@ def Find_Lines():
     for points in lines:
         # Extracted points nested in the list
         x1, y1, x2, y2 = points[0]
+        for iter_x in range(x2 - x1):
+            for iter_y in range(y2 - y1):
+                matrix[x1 + iter_x, y1 + iter_y] = 1 # Does not handle the cases that y2<y1 and x2<x1
         # Draw the lines joing the points
         # On the original image
         cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
@@ -39,6 +43,8 @@ def Find_Lines():
         lines_list.append([(x1, y1), (x2, y2)])
 
     # Save the result image
-    cv2.imshow("Detected lines", image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    return matrix, image
+    #
+    # cv2.imshow("Detected lines", image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
