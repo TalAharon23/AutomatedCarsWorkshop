eed6bbe784308911494a6e93ac653cc3fa4511ba
amirkirshen <88038376+amirkirshen@users.noreply.github.com>
2023-04-10 14:24:30 +0300

    Delete unnecessary video

---
+++ Detection_Handler/Car_Handler.py
@@ -0,0 +1,40 @@
+
+import cv2
+import numpy as np
+
+def Find_Car(frame, matrix, frameSize):
+    # Convert the image to HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # Threshold the image to isolate the yellow color
+    lower_yellow = np.array([20, 100, 100])
+    upper_yellow = np.array([30, 255, 255])
+    mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+    # cv2.imshow('Image', mask)
+
+    # Apply morphological operations to the thresholded image
+    kernel = np.ones((5, 5), np.uint8)
+    mask = cv2.erode(mask, kernel)
+    mask = cv2.dilate(mask, kernel)
+
+    # Detect circles in the thresholded image
+    circles = cv2.HoughCircles(mask, cv2.HOUGH_GRADIENT, 1, 200)
+
+    # Draw circles around the detected yellow circles
+    if circles is not None:
+        circles = np.round(circles[0, :]).astype("int")
+        for (x, y, r) in circles:
+            cv2.circle(mask, (x, y), r, (0, 255, 0), 2)
+
+    # Display the image with the detection results
+    cv2.imshow('Image', mask)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+
+    return matrix, frame
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import numpy as np
 
 import Detection_Handler.Line_Handler as ln_h
-import Detection_Handler.Car_Handler
+import Detection_Handler.Car_Handler as car_h
 import Detection_Handler.Parking_Handler
 import Detection_Handler.Boundaries_Handler
 
@@ -27,17 +27,17 @@ def main():
             pass
         else:
             frame = cv2.resize(frame, frameSize, fx=0, fy=0, interpolation=cv2.INTER_CUBIC)
-            processed_frame = ln_h.Find_Lines(frame, matrix, frameSize)[1]
-            # processed_frame = ln_h.Find_Car(frame, matrix, frameSize)[1]             # Sholmait
+            # processed_data = ln_h.Find_Lines(frame, matrix, frameSize)
+            processed_data = car_h.Find_Car(frame, matrix, frameSize)           # Sholmait
             # processed_frame = ln_h.Find_Boundaries(frame, matrix, frameSize)[1]      # Shriki
             # processed_frame = ln_h.Find_Parking_Lots(frame, matrix, frameSize)[1]    # Jacob
             # Display the resulting frame
-            cv2.imshow('Frame', processed_frame)
+            cv2.imshow('Frame', processed_data[1])
 
             # Press Q on keyboard to exit
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 break
-            out_video.write(processed_frame)
+            out_video.write(processed_data[1])
 
         # Assuming it failed to read only the last frame - video end
         if ret == False:
