d68931ccbd7d8ee4742d08562992cfce84707832
TalAharon23 <talaha@mta.ac.il>
2023-04-10 17:11:51 +0300

    Connect to phone cam in main and interval loop

--- main.py
+++ main.py
@@ -16,19 +16,22 @@ def main():
     # src_video = cv2.VideoCapture('Resources/Amir_Test/sample.mp4')
 
     # Real time video from the upper camera
-    src_video = cv2.VideoCapture(0)
+    # src_video = cv2.VideoCapture(0)
+    url = "http://192.168.1.201:8080/video"
+    src_video = cv2.VideoCapture(url)
     matrix = np.zeros(frameSize)
     mask_line = 'Line'
     mask_border = 'Border'
+    counter = 0
 
     # Processed video from our models (output video in the end of the app)
     out_video = cv2.VideoWriter('Resources/Amir_Test/output_video.avi', cv2.VideoWriter_fourcc(*'DIVX'), 24, frameSize)
-    while (src_video.isOpened()):
+    while src_video.isOpened():
         # Capture frame-by-frame
         ret, frame = src_video.read()
-        if (type(frame) == type(None)):
+        if type(frame) == type(None):
             pass
-        else:
+        elif counter % 3 == 0:
             frame = cv2.resize(frame, frameSize, fx=0, fy=0, interpolation=cv2.INTER_CUBIC)
             processed_frame = ln_h.Find_Lines(frame, matrix, frameSize, mask_line)[1]
             # processed_frame = car_h.Find_Car(frame, matrix, frameSize)[1]             # Sholmait
@@ -44,8 +47,14 @@ def main():
                 break
             out_video.write(processed_frame)
 
+        counter += 1
+
         # Assuming it failed to read only the last frame - video end
         if not ret:
+            break
+
+        q = cv2.waitKey(1)
+        if q == ord("q"):
             break
 
     out_video.release()
