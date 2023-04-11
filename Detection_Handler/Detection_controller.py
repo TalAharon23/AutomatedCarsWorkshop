2271959aaa318c6cca047206450ba698667f41ef
amirkirshen <88038376+amirkirshen@users.noreply.github.com>
2023-04-11 12:10:01 +0300

    refactor to OOP version 2

--- Detection_Handler/Detection_controller.py
+++ Detection_Handler/Detection_controller.py
@@ -15,6 +15,9 @@ val_dict = {
     "Parking_slot": 3,
     "Robot": 4
 }
+mask_line = 'Path'
+mask_border = 'Border'
+url = "http://192.168.1.250:8080/video"
 
 
 class Singleton(type):
@@ -26,38 +29,43 @@ class Singleton(type):
 
 
 # main class
-class Detection_controller(BaseClass, metaclass=Singleton):
+class Detection_controller(metaclass=Singleton):
     # Use this line for saved video:
     # src_video = cv2.VideoCapture('Resources/Amir_Test/sample.mp4')
-
     # Real time video from the phone camera
-    url = "http://192.168.1.215:8080/video"
-    src_video = cv2.VideoCapture(url)
-    matrix = np.zeros(frameSize)
-    mask_line = 'Path'
-    mask_border = 'Border'
-    counter = 0
-    num_of_frame_for_stabiliztion = 0
+    # src_video = None
+    # matrix = None
+    # num_of_frame_for_stabiliztion = 0
     # Processed video from our models (output video in the end of the app)
-    out_video = cv2.VideoWriter('Resources/Amir_Test/output_video.avi', cv2.VideoWriter_fourcc(*'DIVX'), 24, frameSize)
+    # out_video = None
+
+    def __init__(self):
+        self.num_of_frame_for_stabiliztion = 0
+        self.counter = 0
+        self.src_video = cv2.VideoCapture(url)
+        self.out_video = cv2.VideoWriter('Resources/Amir_Test/output_video.avi', cv2.VideoWriter_fourcc(*'DIVX'), 24,
+                                    frameSize)
+        self.matrix = np.zeros(frameSize)
+        self.frame_array = []
 
     def scan_video(self):
-        while (src_video.isOpened()):
+        while (self.src_video.isOpened()):
             # Capture frame-by-frame
-            ret, frame = src_video.read()
-            if type(frame) == type(None):
-                pass
-            elif counter % 3 == 0:
-                self.get_avg_matix()
-                out_video.write(processed_frame)
-            # Press Q on keyboard to exit
-            if cv2.waitKey(25) & 0xFF == ord('q'):
-                break
-            counter += 1
+            ret, frame = self.src_video.read()
             # Assuming it failed to read only the last frame - video end
             if not ret:
                 break
 
+            if self.counter == 6:
+                # self.matrix = bd_h.Create_Template(self.frame_array)
+                pass
+            elif self.counter < 6:
+                self.frame_array.append(self.scan_frame(frame))
+            elif self.counter % 3 == 0:
+                processed_frame = self.scan_frame(frame)[0]
+                self.out_video.write(processed_frame)
+
+            self.counter += 1
             q = cv2.waitKey(1)
             if q == ord("q"):
                 break
@@ -68,17 +76,14 @@ class Detection_controller(BaseClass, metaclass=Singleton):
         # Closes all the windows currently opened.
         cv2.destroyAllWindows()
 
-    def get_avg_matix(self, fram_array):
-        template_data = True, matrix  # .Create_Template(frame, matrix, frameSize, val_dict)
-        if template_data[0] == True:
-            matrix = template_data[1]
-
+    def scan_frame(self, frame):
         frame = cv2.resize(frame, frameSize, fx=0, fy=0, interpolation=cv2.INTER_CUBIC)
-        processed_frame = ln_h.Find_Lines(frame, matrix, frameSize, mask_line, val_dict)[1]  # Find lines
-        processed_frame = ln_h.Find_Lines(frame, matrix, frameSize, mask_border, val_dict)[1]  # Find borders
-        processed_frame, matrix = park_h.Find_Parking(frame, matrix, frameSize)  # Find parking spot
-        # processed_frame = car_h.Find_Car(frame, matrix, frameSize)[1]             # Sholmait
+        processed_frame = ln_h.Find_Lines(frame, self.matrix, frameSize, mask_line, val_dict)[1]  # Find lines
+        processed_frame = ln_h.Find_Lines(frame, self.matrix, frameSize, mask_border, val_dict)[1]  # Find borders
+        processed_frame, matrix = park_h.Find_Parking(frame, self.matrix, frameSize)  # Find parking spot
+        # processed_frame = car_h.Find_Car(frame, matrix, frameSize)[1]
+
         # Display the resulting frame
         cv2.imshow('Frame', processed_frame)
 
-        return processed_frame
+        return processed_frame, matrix
