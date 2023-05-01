f4848deda0a339dde5a21ce0b81d59efbcb0a734
Roni Shulman <101701603+RoniShulmann@users.noreply.github.com>
2023-05-01 18:22:11 +0300

    Roni: Finish with UI (#24)
    * Finish with UI
    
    * Merge with main

--- Detection_Handler/Detection_controller.py
+++ Detection_Handler/Detection_controller.py
@@ -2,13 +2,14 @@ import cv2
 import glob
 import os
 import numpy as np
+import tkinter as tk
 
 import Detection_Handler.Line_Handler as ln_h
 import Detection_Handler.Car_Handler as car_h
 import Detection_Handler.Parking_Handler as park_h
 import Detection_Handler.Boundaries_Handler as bd_h
 
-frameSize = (700, 500)
+frameSize = (600, 500)
 val_dict = {
     "Border": 1,
     "Path": 2,
@@ -97,3 +98,27 @@ class Detection_controller(metaclass=Singleton):
 
     def get_matrix_size(self):
         return frameSize
+
+    def create_buttons(self):
+        def on_button1_click():
+            print("Button 1 clicked!")
+
+        def on_button2_click():
+            print("Button 2 clicked!")
+
+        root = tk.Tk()
+        root.geometry("30x500+83+103")
+        root.wm_attributes("-topmost", 1)
+
+        # Create Button 1 with text "Button 1" and bind it to the on_button1_click() function
+        button1 = tk.Button(root, text="Parking", command=on_button1_click, width=45, height=3, bg="green",
+                            font=("Arial", 16))
+        button1.pack()
+
+        # Create Button 2 with text "Button 2" and bind it to the on_button2_click() function
+        button2 = tk.Button(root, text="Stop", command=on_button2_click, width=45, height=3, bg="red",
+                            font=("Arial", 16))
+        button2.pack()
+
+        # Start the tkinter event loop
+        root.mainloop()
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import cv2
 import glob
 import os
 import numpy as np
+import threading
+
 
 from ESP32CAM_Car.MovementAPI import move
 import Detection_Handler.Detection_controller as logic
@@ -9,7 +11,13 @@ import Detection_Handler.Detection_controller as logic
 
 def main():
     image_logic = logic.Detection_controller()
-    image_logic.scan_video()
+    # Create a thread for the video scanning process
+    video_thread = threading.Thread(target=image_logic.scan_video)
+    video_thread.start()
+    # Create a thread for the GUI
+    gui_thread = threading.Thread(target=image_logic.create_buttons)
+    gui_thread.start()
+
 
 
 if __name__ == '__main__':
