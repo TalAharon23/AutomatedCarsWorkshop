c3e156e94c12ffa4115fc19945a52d8906d01bc1
amirkirshen <88038376+amirkirshen@users.noreply.github.com>
2023-07-21 16:07:31 +0300

    First changes for using Movement_Handler.py

--- Movement_Handler.py
+++ Movement_Handler.py
@@ -1,6 +1,7 @@
 from ESP32CAM_Car.Movement import move
-from .Detection_Handler.Detection_controller import Detection_controller as DC
+from .Detection_Handler.Detection_controller import Detection_controller
 import Data_Structures
+import BFS_Logic
 
 class MOVE_COMMANDS:
     Forward = "go"
@@ -15,56 +16,82 @@ class DIRECTIONS:
     Right = "Right"
     Down = "Down"
 
+class Movement_Handler():
+
+    def __init__(self):
+        self.Detection_controller               = Detection_controller()
+        self.BFS_Logic                          = BFS()
+        self.car                                = Car()
+        self.car_arrived_to_maneuver_point      = False
+        self.car_arrived_to_destination         = False
+        self.parking_slot_dest                  = None
+        self.parking_slots                      = Parking_Slots()
+
+    def get_car_position(self):
+        return self.car.get_position()
+
+    def get_car_position_in_matrix(matrix):
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == VAL_DICT["Robot"]:
+                    return i, j
+        raise ValueError("Matrix does not contain the car.")
+
+    def make_one_step(self):
+        while
+
+    def set_parking_slot_destination(self):
+        chosen_slot = self.parking_slots[0]
+        nearest_parking_slot_path = None
+        dist_nearest_parking_slot = len(nearest_parking_slot_path)
+        for slot in self.parking_slots:
+            nearest_parking_slot_path = self.BFS_Logic.shortestPath(get_matrix, self.car.get_position(), self.slot)
+            if len(nearest_parking_slot_path) < dist_nearest_parking_slot:
+                dist_nearest_parking_slot = len(nearest_parking_slot_path)
+                chosen_slot = slot
+
+        self.parking_slot_dest = chosen_slot
+
+    # def car_movement(matrix):
+    #     car = Car()
+    #     new_matrix = duplicate_matrix(matrix)
+    #
+    #     while matrix[car.position[0]][car.position[1]] == VAL_DICT["Path"]:
+    #         move(MOVE_COMMANDS.Forward)
+    #
+    #         # Check if the car went out of line or hit an empty cell
+    #         if not car.in_bounds(new_matrix) or new_matrix[car.position[0]][car.position[1]] == VAL_DICT["Empty"]:
+    #             path_positions = find_positions(new_matrix, VAL_DICT["Path"])
+    #             path_positions.remove(car.position)
+    #
+    #             # Try to turn right
+    #             right_pos = car.right()
+    #             if right_pos in path_positions:
+    #                 move(MOVE_COMMANDS.Right)
+    #             else:
+    #                 # Try to turn left
+    #                 left_pos = car.left()
+    #                 if left_pos in path_positions:
+    #                     move(MOVE_COMMANDS.Left)
+    #
+    #         # Update the car position in the new matrix
+    #         new_matrix[car.position[0]][car.position[1]] = VAL_DICT["Robot"]
+    #
+    #     return new_matrix
 
 
-def get_car_position_in_matrix(matrix):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            if matrix[i][j] == VAL_DICT["Robot"]:
-                return i, j
-    raise ValueError("Matrix does not contain the car.")
+    def update_car_direction(car, prev_car_position):
+        """
+        Update the car's direction based on the current and previous positions.
 
-
-def car_movement(matrix):
-    car = Car()
-    new_matrix = duplicate_matrix(matrix)
-
-    while matrix[car.position[0]][car.position[1]] == VAL_DICT["Path"]:
-        move(MOVE_COMMANDS.Forward)
-
-        # Check if the car went out of line or hit an empty cell
-        if not car.in_bounds(new_matrix) or new_matrix[car.position[0]][car.position[1]] == VAL_DICT["Empty"]:
-            path_positions = find_positions(new_matrix, VAL_DICT["Path"])
-            path_positions.remove(car.position)
-
-            # Try to turn right
-            right_pos = car.right()
-            if right_pos in path_positions:
-                move(MOVE_COMMANDS.Right)
-            else:
-                # Try to turn left
-                left_pos = car.left()
-                if left_pos in path_positions:
-                    move(MOVE_COMMANDS.Left)
-
-        # Update the car position in the new matrix
-        new_matrix[car.position[0]][car.position[1]] = VAL_DICT["Robot"]
-
-    return new_matrix
-
-
-def update_car_direction(car, prev_car_position):
-    """
-    Update the car's direction based on the current and previous positions.
-
-    :param car: Car object
-    :param prev_car_position: Tuple of previous car position in matrix
-    """
-    if car.position[0] == prev_car_position[0] and car.position[1] > prev_car_position[1]:
-        car.direction = DIRECTIONS.Right
-    elif car.position[0] == prev_car_position[0] and car.position[1] < prev_car_position[1]:
-        car.direction = DIRECTIONS.Left
-    elif car.position[0] > prev_car_position[0] and car.position[1] == prev_car_position[1]:
-        car.direction = DIRECTIONS.Down
-    elif car.position[0] < prev_car_position[0] and car.position[1] == prev_car_position[1]:
-        car.direction = DIRECTIONS.Up
+        :param car: Car object
+        :param prev_car_position: Tuple of previous car position in matrix
+        """
+        if car.position[0] == prev_car_position[0] and car.position[1] > prev_car_position[1]:
+            car.direction = DIRECTIONS.Right
+        elif car.position[0] == prev_car_position[0] and car.position[1] < prev_car_position[1]:
+            car.direction = DIRECTIONS.Left
+        elif car.position[0] > prev_car_position[0] and car.position[1] == prev_car_position[1]:
+            car.direction = DIRECTIONS.Down
+        elif car.position[0] < prev_car_position[0] and car.position[1] == prev_car_position[1]:
+            car.direction = DIRECTIONS.Up
--- UI/main_screen.py
+++ UI/main_screen.py
@@ -1,74 +1,25 @@
-# import tkinter as tk
-# import cv2
-# from PIL import Image, ImageTk
-#
-# root = tk.Tk()
-# root.geometry("1300x500")
-#
-#
-# def parkingButton_click():
-#     print("Parking Button clicked")
-#
-# def stopButton_click():
-#     print("Stop Button clicked")
-#
-#
-# parkingButton = tk.Button(root, text="Parking", command=parkingButton_click, height=4, width=10, bg="blue", fg="white", font=("Arial", 30))
-# parkingButton.pack(pady=10)
-#
-# stopButton = tk.Button(root, text="Stop", command=stopButton_click, height=4, width=10, bg="blue", fg="white", font=("Arial", 30))
-# stopButton.pack(pady=10)
-#
-# canvas1 = tk.Canvas(root, width=320, height=240)
-# canvas1.place(x=0, y=0, anchor="nw")
-# canvas1.config(width=500, height=457)
-#
-# canvas2 = tk.Canvas(root, width=320, height=240)
-# canvas2.place(x=0, y=0, anchor="ne")
-# canvas2.config(width=500, height=457)
-#
-# cap1 = cv2.VideoCapture(0)
-# cap2 = cv2.VideoCapture(1)  # use different capture devices for each canvas
-#
-#
-# def update_canvas(canvas, cap):
-#     ret, frame = cap.read()
-#     if ret:
-#         # convert the frame to a PIL Image object
-#         image = Image.fromarray(frame)
-#
-#         # convert the PIL Image object to a PhotoImage object that can be displayed on a Canvas
-#         photo = ImageTk.PhotoImage(image)
-#
-#         # display the PhotoImage on the Canvas
-#         canvas.create_image(0, 0, anchor=tk.NW, image=photo)
-#
-#         # keep a reference to the PhotoImage to prevent it from being garbage collected
-#         canvas.photo = photo
-#
-#     # schedule the next update after 10 milliseconds
-#     root.after(10, update_canvas, canvas, cap)
-#
-#
-# # start the update loop for both canvases
-# update_canvas(canvas1, cap1)
-# update_canvas(canvas2, cap2)
-#
-# button_frame = tk.Frame(root)
-# button_frame.pack(pady=10)
-#
-# button1 = tk.Button(button_frame, text="Parking", bg="green", width=10, height=2)
-# button1.pack(side=tk.LEFT, padx=10)
-#
-# button2 = tk.Button(button_frame, text="Stop", bg="red", width=10, height=2)
-# button2.pack(side=tk.LEFT, padx=10)
-#
-# root.geometry("1300x600")
-# button1.place(x=100, y=300)
-# button2.place(x=300, y=300)
-#
-#
-#
-# root.mainloop()  # start the main event loop
-#
-#
+import tkinter as tk
+
+def create_buttons(self):
+    def on_button1_click():
+        print("Button 1 clicked!")
+
+    def on_button2_click():
+        print("Button 2 clicked!")
+
+    root = tk.Tk()
+    root.geometry("30x500+83+103")
+    root.wm_attributes("-topmost", 1)
+
+    # Create Button 1 with text "Button 1" and bind it to the on_button1_click() function
+    button1 = tk.Button(root, text="Parking", command=on_button1_click, width=45, height=3, bg="green",
+                        font=("Arial", 16))
+    button1.pack()
+
+    # Create Button 2 with text "Button 2" and bind it to the on_button2_click() function
+    button2 = tk.Button(root, text="Stop", command=on_button2_click, width=45, height=3, bg="red",
+                        font=("Arial", 16))
+    button2.pack()
+
+    # Start the tkinter event loop
+    root.mainloop()
