80a119946c36c9c0af79c74d7c73b55b9d30d7ee
TalAharon23 <talaha@mta.ac.il>
2023-08-05 13:58:00 +0300

    Roni and Tal: Merge with Kirsh

--- Detection_Handler/Detection_controller.py
+++ Detection_Handler/Detection_controller.py
@@ -1,6 +1,4 @@
 import cv2
-import glob
-import os
 import numpy as np
 
 import Data_Structures
@@ -33,60 +31,64 @@ class Singleton(type):
 
 # main class
 class Detection_controller(metaclass=Singleton):
-    # Use this line for saved video:
-    # src_video = cv2.VideoCapture('Resources/Amir_Test/sample.mp4')
-    # Real time video from the phone camera
-    # src_video = None
-    # matrix = None
-    # num_of_frame_for_stabiliztion = 0
-    # Processed video from our models (output video in the end of the app)
-    # out_video = None
 
     def __init__(self):
-        self.num_of_frame_for_stabiliztion = 0
-        self.counter = 0
-        self.src_video = cv2.VideoCapture(url)
-        self.out_video = cv2.VideoWriter('Resources/Amir_Test/output_video.avi', cv2.VideoWriter_fourcc(*'DIVX'), 24,
-                                         frameSize)
-        self.matrix = np.zeros(frameSize)
-        self.frame_array = []
+        self.num_of_frame_for_stabiliztion          = 0
+        self.counter                                = 0
+        self.src_video                              = cv2.VideoCapture(url)
+        self.out_video                              = cv2.VideoWriter('Resources/Amir_Test/output_video.avi',
+                                                                      cv2.VideoWriter_fourcc(*'DIVX'), 24,frameSize)
+        self.matrix                                 = np.zeros(frameSize)
+        self.frame_array                            = []
+        self.flag                                   = True
+        self.framenum                               = 0
 
-    def scan_video(self):
+
+    def scan_video(self, car):
         while (self.src_video.isOpened()):
             # Capture frame-by-frame
             ret, frame = self.src_video.read()
             # Assuming it failed to read only the last frame - video end
             if not ret:
                 break
-
             if self.counter == 6:
                 # self.matrix = bd_h.Create_Template(self.frame_array)
                 pass
             elif self.counter < 6:
-                self.frame_array.append(self.scan_frame(frame))
+                self.frame_array.append(self.scan_frame(frame, car))
             elif self.counter % 3 == 0:
-                processed_frame = self.scan_frame(frame)[0]
+                processed_frame = self.scan_frame(frame, car)[0]
                 self.out_video.write(processed_frame)
-
             self.counter += 1
             q = cv2.waitKey(1)
             if q == ord("q"):
                 break
-        out_video.release()
 
         self.out_video.release()
         # release the src_video capture object
-        src_video.release()
+        self.src_video.release()
         # Closes all the windows currently opened.
         cv2.destroyAllWindows()
 
-    def scan_frame(self, frame):
+    def print_matrix(self, matrix):
+        """for rows in range(matrix.shape[0]):
+            for cols in range(matrix.shape[1]):
+                print(str(matrix[rows][cols]) + " ")
+            print()"""
+        np.set_printoptions(threshold=np.inf)
+
+        # print the matrix
+        # print(matrix)
+        matrix_scaled = cv2.normalize(matrix, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+        cv2.imwrite("matrix_image" + str(self.framenum) + ".jpg", matrix_scaled)
+
+    def scan_frame(self, frame, car):
         frame = cv2.resize(frame, frameSize, fx=0, fy=0, interpolation=cv2.INTER_CUBIC)
         origin_frame = frame.copy()
-        processed_frame = ln_h.Find_Lines(frame, self.matrix, frameSize, mask_line, val_dict)[1]  # Find lines
+        # processed_frame = ln_h.Find_Lines(frame, self.matrix, frameSize, mask_line, val_dict)[1q]  # Find path
         processed_frame = ln_h.Find_Lines(frame, self.matrix, frameSize, mask_border, val_dict)[1]  # Find borders
-        processed_frame, matrix = park_h.Find_Parking(frame, self.matrix, frameSize)  # Find parking spot
-        # processed_frame = car_h.Find_Car(frame, matrix, frameSize)[1]
+        # processed_frame, matrix = park_h.Find_Parking(frame, self.matrix, frameSize, val_dict)  # Find parking spot
+        processed_frame = car_h.Find_Car(frame, self.matrix, frameSize, car)[1]
 
         matrix_scaled = cv2.normalize(self.matrix, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
         cv2.imwrite('color_img.jpg', matrix_scaled)
@@ -97,12 +99,10 @@ class Detection_controller(metaclass=Singleton):
 
         # Display the concatenated frame
         cv2.imshow('Autonomous Car', h_concat)
-        return processed_frame, matrix
+        return processed_frame, self.matrix
 
     def get_matrix(self):
         return self.matrix
 
     def get_matrix_size(self):
         return frameSize
-
-
