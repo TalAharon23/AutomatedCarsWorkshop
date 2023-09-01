7f48b88f19025ad67bb0518119cf95a39045caaa
TalAharon23 <talaha@mta.ac.il>
2023-09-01 12:36:20 +0300

    Parking and stop buttons working

--- main.py
+++ main.py
@@ -7,24 +7,21 @@ import tkinter as tk
 from PIL import Image, ImageTk
 from ESP32CAM_Car.MovementAPI import move
 import Detection_Handler.Detection_controller as logic
-#import UI.main_screen as UI
-import UI.UI_second_test as UI
+import UI.main_screen as UI
 import Movement_Handler
 
 # Will be deleted and moved into Movement_Handler
 import Data_Structures as DS
 
 
-
 def main():
     root = tk.Tk()
-    app = UI.CarApp(root, "Car Project UI")
+    app = UI.AutoPark(root, "AutoPark")
     root.mainloop()
 
-
-    #image_logic = Movement_Handler.Movement_Handler()
+    # image_logic = Movement_Handler.Movement_Handler()
     # Create a thread for the video scanning process
-    #image_logic.start_car_parking_session()
+    # image_logic.start_car_parking_session()
     # video_thread = threading.Thread(target=image_logic.start_car_parking_session, args=[])
     # video_thread.start()
     # Create a thread for the GUI
